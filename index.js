dbcd58261f5964cd0ae6b964ec4c3fa6ef336023
IAmHim89 <computertech0418@gmail.com>
2024-07-23 13:49:53 -0400

    Assignment

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 //Copy and paste your code from creating-json to start this project
+//express library
+const express = require("express");
+
+//cors intialization
+const cors = require("cors");
+
+//variable holding the activation of express
+const app = express();
+
+//json file
+const api = "/json.json";
+
+app.use(express.json());
+app.use(cors());
+//port server #
+const PORT = 3000;
+
+//get response for json file/path
+app.get("/", (req, res) => {
+  res.send({ api });
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on PORT ${PORT}`);
+});
+
 // Don't forget to copy your "api" directory and contents as well
